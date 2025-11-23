cecebd4456b279200ff0bf49070ff4c803f30289
Jack Compton <72893058+comptun@users.noreply.github.com>
2025-11-23 04:39:09 +0000

    Update

--- src/Register.jsx
+++ src/Register.jsx
@@ -13,7 +13,7 @@ function Register() {
     }
 
     async function handleSubmit() {
-        const res = await fetch("api/users", {
+        const res = await fetch("/api/users", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(inputs)
