7607da6fc579d62090f54ec16e0ba467aeb58422
Jack Compton <72893058+comptun@users.noreply.github.com>
2025-11-25 14:35:05 +0000

    Logo change

--- infra/d1.tf
+++ infra/d1.tf
@@ -3,5 +3,7 @@ resource "cloudflare_d1_database" "radio360db" {
   name = "radio360"
   jurisdiction = "eu"
   primary_location_hint = "wnam"
-  read_replication = {}
+  read_replication = {
+    mode = "disabled"
+  }
 }
