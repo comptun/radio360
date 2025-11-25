906c9510386a467dc6f424e83aaa445813e803ce
Jack Compton <72893058+comptun@users.noreply.github.com>
2025-11-25 18:43:47 +0000

    Added turnstile to infra

--- infra/turnstile.tf
+++ infra/turnstile.tf
@@ -10,6 +10,7 @@ resource "cloudflare_turnstile_widget" "login_radio360_turnstile_widget" {
   region = "world"
 }
 
+
 resource "cloudflare_turnstile_widget" "register_radio360_turnstile_widget" {
   account_id = var.account_id
   domains = ["radio360-bjp.pages.dev"]
