a4c2b85dadb2e9efcf1d38f638156d5b86477a4b
Jack Compton <72893058+comptun@users.noreply.github.com>
2025-11-25 15:00:11 +0000

    Update

--- infra/d1.tf
+++ infra/d1.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_d1_database" "radio360db" {
   account_id = var.account_id
-  name = "radio360"
+  name = "radio360-d1-db"
   jurisdiction = "eu"
   primary_location_hint = "wnam"
   read_replication = {
